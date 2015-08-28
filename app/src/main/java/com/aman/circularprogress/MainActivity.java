2da2d9dbbf27ea272922c0366967e07b1ec1b714
aman <amandeep400@gmail.com>
2015-08-28 21:32:43 +0530

    First version of progress bar

---
+++ app/src/main/java/com/aman/circularprogress/MainActivity.java
@@ -0,0 +1,46 @@
+package com.aman.circularprogress;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final CircularProgress progress = (CircularProgress) findViewById(R.id.rating_progress);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progress.setProgressWithAnimation(95);
+            }
+        }, 2000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
